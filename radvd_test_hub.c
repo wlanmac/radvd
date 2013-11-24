7c73c6cd311f71b68dc744f8a36a95d3e6562aaf
Reuben <reubenhwk@gmail.com>
2013-11-24 01:24:45 -0600

    added get_opts to radvd_test_hub.c

--- radvd_test_hub.c
+++ radvd_test_hub.c
@@ -1,11 +1,17 @@
 
+#include "config.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <net/if_arp.h>
 #include <sys/select.h>
+
+#ifdef HAVE_GETOPT_H
+# include <getopt.h>
+#endif
 
 #include "tuntap.h"
 
@@ -39,15 +45,98 @@ void write_config(char const * dev, char const * mode)
 
 }
 
+#ifdef HAVE_GETOPT_LONG
+
+static char usage_str[] = {
+"\n"
+"  -C, --config=PATH       Sets the config file.  Default is /etc/radvd.conf.\n"
+"  -h, --help              Print this help message then quit.\n"
+"  -n, --number=NUM        Sets the number of interfaces to create.\n"
+"  -t, --type=string       Sets the type of interfaces to create.  Valid values are [tun|tap].\n"
+};
+
+static struct option prog_opt[] = {
+	{"config", 1, 0, 'C'},
+	{"help", 0, 0, 'h'},
+	{"number", 1, 0, 'n'},
+	{"type", 1, 0, 't'},
+	{NULL, 0, 0, 0}
+};
+
+#else
+
+static char usage_str[] = {
+	"[-C config_file] [-h] [-n number] [-t type]\n"
+};
+
+#endif
+
+static char *pname;
+void usage(void);
+void usage(void)
+{
+	fprintf(stderr, "usage: %s %s\n", pname, usage_str);
+	exit(1);
+}
+
 int main(int argc, char * argv[])
 {
-#define IFACE_COUNT 5
 	int sock;
+	int type = IFF_TAP;
 	struct ifreq	ifr;
-	int fds[IFACE_COUNT];
-	int tunnels = IFACE_COUNT;
+	int fds[1000];
+	int tunnels = 1;
+	int i;
+	int c;
 	fd_set readset;
 	FD_ZERO(&readset);
+	char const * conf_file = "radvd.conf";
+#ifdef HAVE_GETOPT_LONG
+	int opt_idx;
+#endif
+
+	pname = ((pname=strrchr(argv[0],'/')) != NULL)?pname+1:argv[0];
+	/* parse args */
+#define OPTIONS_STR "n:t:h"
+#ifdef HAVE_GETOPT_LONG
+	while ((c = getopt_long(argc, argv, OPTIONS_STR, prog_opt, &opt_idx)) > 0)
+
+#else
+	while ((c = getopt(argc, argv, OPTIONS_STR)) > 0)
+#endif
+	{
+		switch (c) {
+		case 'C':
+			conf_file = optarg;
+			break;
+		case 't':
+			if (strcmp(optarg, "tun"))
+				type = IFF_TUN;
+			else if(strcmp(optarg, "tap"))
+				type = IFF_TAP;
+			else
+				usage();
+			break;
+		case 'n':
+			tunnels = atoi(optarg);
+			if (tunnels > (sizeof(fds)/sizeof(fds[0]))) {
+				fprintf(stderr, "number of tunnels is capped at %lu", sizeof(fds)/sizeof(fds[0]));
+				tunnels = sizeof(fds)/sizeof(fds[0]);
+			}
+			break;
+		case 'h':
+			usage();
+#ifdef HAVE_GETOPT_LONG
+		case ':':
+			fprintf(stderr, "%s: option %s: parameter expected\n", pname,
+				prog_opt[opt_idx].name);
+			exit(1);
+#endif
+		case '?':
+			exit(1);
+		}
+	}
+
 
 	sock = open_icmpv6_socket();
 	if (sock < 0) {
@@ -55,17 +144,17 @@ int main(int argc, char * argv[])
 		exit(1);
 	}
 
-	unlink("radvd.conf");
+	unlink(conf_file);
 
-	while (tunnels--) {
+	for (i = 0; i < tunnels; ++i) {
 		char dev[IFNAMSIZ] = {""};
 
-		fds[tunnels] = tun_alloc(dev, IFF_TAP);
-		if (fds[tunnels] < 0) {
+		fds[i] = tun_alloc(dev, type);
+		if (fds[i] < 0) {
 			perror("tun_alloc failed");
 			exit(1);
 		}
-		FD_SET(fds[tunnels], &readset);
+		FD_SET(fds[i], &readset);
 #if 0
 		if(ioctl(fd, TUNSETPERSIST, 1) < 0){
 			perror("enabling TUNSETPERSIST");
@@ -108,13 +197,12 @@ int main(int argc, char * argv[])
 		printf("interface %s ready...\n", dev);
 	}
 
-	printf("radvd.conf written\n");
+	printf("%s written with %d interface definitions.\n", conf_file, tunnels);
 
 	while (1) {
-		int rc = select(sizeof(fds)/sizeof(fds[0]), &readset, 0, 0, 0);
+		int rc = select(tunnels, &readset, 0, 0, 0);
 		if (rc > 0) {
-			int i = 0;
-			for (; i < sizeof(fds)/sizeof(fds[0]); ++i) {
+			for (i = 0; i < tunnels; ++i) {
 				if (FD_ISSET(fds[i], &readset)) {
 					char buffer[1500];
 					int count = read(fds[i], buffer, sizeof(buffer));
